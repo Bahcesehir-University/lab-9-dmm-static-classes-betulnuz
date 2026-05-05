f6fce4e66a6d6956aee80f1392765e5cd9070369
betül Nur Uzan <161211369+betulnuz@users.noreply.github.com>
2026-05-05 12:30:25 +0300

    Implement Tracker and IntArray functionalities

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -92,6 +92,8 @@ public:
 // STATIC MEMBER INITIALIZATION
 // ================================================================
 
+int Tracker::objectCount = 0;
+
 // TODO 1: Initialize Tracker's static member variable
 // Hint: int Tracker::objectCount = ???;
 
@@ -101,19 +103,23 @@ public:
 // ================================================================
 
 void Tracker::objectCreated() {
+    objectCount++;
     // TODO 2: Increment objectCount
 }
 
 void Tracker::objectDestroyed() {
+    objectCount--;
     // TODO 3: Decrement objectCount
 }
 
 int Tracker::getActiveCount() {
+    return objectCount;
     // TODO 4: Return objectCount
     return 0;
 }
 
 void Tracker::resetCount() {
+    objectCount=0;
     // TODO 5: Reset objectCount to 0
 }
 
@@ -123,6 +129,13 @@ void Tracker::resetCount() {
 
 // Constructor
 IntArray::IntArray(int cap) {
+    
+    capacity = cap;
+    count = 0;
+    data = new int[capacity];
+
+    Tracker::objectCreated();
+    
     // TODO 6: Allocate dynamic array of size cap using 'new'
     //         Initialize capacity, count
     //         Notify Tracker that an object was created
@@ -131,6 +144,9 @@ IntArray::IntArray(int cap) {
 
 // Destructor
 IntArray::~IntArray() {
+    delete[] data;
+
+    Tracker::objectDestroyed();
     // TODO 7: Free the dynamic array using 'delete[]'
     //         Notify Tracker that an object was destroyed
 
@@ -138,22 +154,28 @@ IntArray::~IntArray() {
 
 // Copy Constructor
 IntArray::IntArray(const IntArray& other) {
+    capacity=other.capacity;
+    count=other.count;
+    data=new int[capacity];
+    
+    for (int i = 0; i < count; i++) {
+    data[i] = other.data[i];
+    }
     // TODO 8: Deep copy - allocate new memory and copy elements
     //         Don't forget to copy capacity and count
     //         Notify Tracker that an object was created
 
 }
 
-// Copy Assignment Operator
 IntArray& IntArray::operator=(const IntArray& other) {
-    // TODO 9: Implement copy assignment
-    //         1. Check for self-assignment (this != &other)
-    //         2. Delete old memory
-    //         3. Allocate new memory
-    //         4. Copy all elements, capacity, and count
-    //         5. Return *this
-    //         NOTE: Do NOT call Tracker here (object already exists)
-
+    if (this != &other) {               // self-assignment guard
+        delete[] data;                  // free old memory
+        capacity = other.capacity;
+        count = other.count;
+        data = new int[capacity];       // allocate new memory
+        for (int i = 0; i < count; i++)
+            data[i] = other.data[i];
+    }
     return *this;
 }
 
@@ -162,6 +184,13 @@ bool IntArray::add(int value) {
     // TODO 10: If count < capacity, add value at data[count],
     //          increment count, return true.
     //          Otherwise return false.
+
+    if (count < capacity) {
+        data[count] = value;
+        count++;
+        return true;
+    }
+
     return false;
 }
 
@@ -169,33 +198,45 @@ bool IntArray::add(int value) {
 int IntArray::get(int index) const {
     // TODO 11: If index is valid (0 <= index < count), return data[index].
     //          Otherwise return -1.
+
+    if (index >= 0 && index < count) {
+        return data[index];
+    }
+
     return -1;
 }
 
 // Size
 int IntArray::size() const {
     // TODO 12: Return count
-    return 0;
+    return count;
 }
 
 // Capacity
 int IntArray::getCapacity() const {
     // TODO 13: Return capacity
-    return 0;
+    return capacity;
 }
 
 // isEmpty
 bool IntArray::isEmpty() const {
     // TODO 14: Return true if count == 0
-    return true;
+    return count == 0;
 }
+
 
 // Remove last element
 bool IntArray::removeLast() {
     // TODO 15: If not empty, decrement count and return true.
     //          Otherwise return false.
+
+    if (!isEmpty()) {
+        count--;
+        return true;
+    }
     return false;
 }
+
 
 // ================================================================
 // MAIN FUNCTION
